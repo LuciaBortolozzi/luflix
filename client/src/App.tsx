5a369ce57c6daee021910f6a73b79a2d531c9471
ASPIRE7\lucia <luciabortolozzi@gmail.com>
2024-09-29 13:13:57 +0200

    [FEATURE][06]: Browse page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import PlansPage from "./pages/PlansPage";
+import BrowsePage from "./pages/BrowsePage";
 
 function App() {
   return (
     <>
       <div>
-        <PlansPage />
+        <BrowsePage />
       </div>
     </>
   );
---
+++ client/src/pages/BrowsePage.tsx
@@ -0,0 +1,15 @@
+import Billboard from "../components/Billboard";
+import MovieList from "../components/MovieList";
+import NavBar from "../components/NavBar";
+
+export default function BrowsePage() {
+  return (
+    <div>
+      <NavBar />
+      <Billboard />
+      <div className="pb-5">
+        <MovieList />
+      </div>
+    </div>
+  );
+}
