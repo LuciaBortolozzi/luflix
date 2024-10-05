9e955bb20336c1c37585dfe3e332f02b1259546d
ASPIRE7\lucia <luciabortolozzi@gmail.com>
2024-10-05 16:32:19 +0200

    [FEATURE][24]: Rendering plans

--- client/src/components/PlanCard.tsx
+++ client/src/components/PlanCard.tsx
@@ -1,26 +1,60 @@
-import { CheckIcon } from "@heroicons/react/24/outline";
+import { CheckIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Plan } from "../hooks/usePlans";
 
-export default function PlanCard() {
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "EUR",
+});
+export default function PlanCard({ plan }: { plan: Plan }) {
+  const { name, price, canViewHD, canDownload } = plan;
+
   return (
-    <div className="border rounded p-3 h-[350px] w-full pointer mr-3 bg-black text-white">
-      <div className="rounded bg-gradient-to-r from-red-500 to-red-700 w-full p-3 font-bold">
-        <h3 className="text-2xl">Basic</h3>
+    <div className="border rounded p-3 h-[350px] w-full pointer mr-3">
+      <div className="rounded bg-gradient-to-r from-red-500 to-red-500 w-full p-3 text-white font-bold">
+        <h3 className="text-2xl">{name}</h3>
+        <p className="font-light">{formatter.format(price.amount / 100)}</p>
       </div>
-      <div className="border-b border-gray-700 py-4 flex items-center">
+
+      <div className="border-b py-4 flex text-reg items-center">
         <div className="w-6 h-6 rounded-full text-sm bg-red-500 flex items-center justify-center">
           <CheckIcon color="white" width={15} fontWeight={900} />
         </div>
         <div className="ml-3">
-          <h3 className="text-gray-400">Monthly price</h3>
-          <h3 className="font-semibold">$5.00</h3>
+          <h3 className="text-gray-600">Monthly price</h3>
+          <h3 className="font-semibold">
+            {formatter.format(price.amount / 100)}
+          </h3>
         </div>
       </div>
-      <div className="border-b border-gray-700 py-4 flex items-center">
-        <div className="w-6 h-6 rounded-full text-sm bg-red-500 flex items-center justify-center">
-          <CheckIcon color="white" width={15} fontWeight={900} />
-        </div>
+
+      <div className="border-b py-4 flex text-reg items-center">
+        {canViewHD ? (
+          <div className="w-6 h-6 rounded-full text-sm bg-red-500 flex items-center justify-center">
+            <CheckIcon color="white" width={15} fontWeight={900} />
+          </div>
+        ) : (
+          <div className="w-6 h-6 rounded-full text-sm bg-red-500 flex items-center justify-center">
+            <XMarkIcon color="white" width={15} fontWeight={900} />
+          </div>
+        )}
         <div className="ml-3">
-          <h3 className="text-gray-400">Downloads</h3>
+          <h3 className="text-gray-600">HD</h3>
+          <h3 className="font-semibold">Included</h3>
+        </div>
+      </div>
+
+      <div className="border-b py-4 flex text-reg items-center">
+        {canDownload ? (
+          <div className="w-6 h-6 rounded-full text-sm bg-red-500 flex items-center justify-center">
+            <CheckIcon color="white" width={15} fontWeight={900} />
+          </div>
+        ) : (
+          <div className="w-6 h-6 rounded-full text-sm bg-red-500 flex items-center justify-center">
+            <XMarkIcon color="white" width={15} fontWeight={900} />
+          </div>
+        )}
+        <div className="ml-3">
+          <h3 className="text-gray-600">Downloads</h3>
           <h3 className="font-semibold">Included</h3>
         </div>
       </div>
